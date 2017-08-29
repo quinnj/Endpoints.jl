3969f6ed70037d8a3c7867b8044e19964661e6f2
sarv27 <sarvjeet.ghotra@myntra.com>
2017-08-29 19:06:58 +0530

    Add routing functionalities. Delete previous files since they had major issues.

---
+++ src/Routing.jl
@@ -0,0 +1,58 @@
+module Routing
+
+using HTTP
+include("route.jl")
+
+export Handler, Router, handle, register!, setdefaulthandler, Vars
+
+abstract type Handler end
+
+handle(h, req, res) = h.func(req, resp)
+
+const FourOhFour = ((req, resp) -> Response(404))
+
+mutable struct Router
+    routes::Array{Route}
+    defaulthandler # TODO: restrict its interface to Handler type
+    function Router()
+        r = new([], FourOhFour)
+        return r
+    end
+end
+
+function handle(rr::Router, req, res)
+    #find a match for the request
+    handler = match_handler(rr, req)
+    return handler(req, res)
+end
+
+function register!(rr::Router, handler; path=nothing, methods=nothing,
+            host=nothing, headers=[])
+    route = Route(handler, path, methods, host, headers)
+    push!(rr.routes, route)
+end
+
+function match_handler(rr::Router, req)
+    # match against all the registered paths
+    for route in rr.routes
+        if matchroute(route, req)
+            global Vars = Dict{String, Any}()
+            for m in route.matchers
+                if typeof(m) == Regexpmatcher
+                    merge!(Vars, setvarnames(m, req))
+                end
+            end
+            return route.handler
+        end
+    end
+    return rr.defaulthandler
+end
+
+function setdefaulthandler(rr::Router, handler)
+    rr.defaulthandler = handler
+end
+
+# to access the variable values from the path and host of the matched route
+Vars = Dict{String, Any}()
+
+end
